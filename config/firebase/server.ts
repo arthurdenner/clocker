94393ce568f429e1b0a76c3674be975d4887182f
Arthur Denner <arthurdenner7@gmail.com>
2021-07-04 00:06:03 +0200

    refactor: migrate Firebase files to TypeScript

--- config/firebase/server.ts
+++ config/firebase/server.ts
@@ -3,7 +3,6 @@ import admin from 'firebase-admin';
 const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_CERT);
 const credential = admin.credential.cert(serviceAccount);
 
-export const app = firebaseServer.apps.length
-  ? firebaseServer.app()
+export const app = admin.apps.length
+  ? admin.app()
   : admin.initializeApp({ credential });
-
