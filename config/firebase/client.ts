55702a42c3235ff1083813b4c6685ecfe449cb37
Arthur Denner <arthurdenner7@gmail.com>
2022-05-10 17:48:28 +0200

    feat: move Firebase client config to env variable

--- config/firebase/client.ts
+++ config/firebase/client.ts
@@ -1,16 +1,7 @@
 import firebaseApp from 'firebase/app';
 import 'firebase/auth';
 
-// TODO: JSON.parse from env variable
-const firebaseConfig = {
-  apiKey: 'AIzaSyCFWWR9o1f9uVsnXAgpUu1kiV3juL-scH4',
-  authDomain: 'clocker-arthurdenner.firebaseapp.com',
-  projectId: 'clocker-arthurdenner',
-  storageBucket: 'clocker-arthurdenner.appspot.com',
-  messagingSenderId: '123828951279',
-  appId: '1:123828951279:web:89ba53f058263ce29f231a',
-  measurementId: 'G-KSV6RYJ190',
-};
+const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG);
 
 export const firebase = firebaseApp.apps.length
   ? firebaseApp.app()
